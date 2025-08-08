c6c9996462d4777238e950ada5a99892ba943553
Kartikey chauhan <itskartikeyy15@gmail.com>
2025-08-09 00:41:14 +0530

    creating react vite

---
+++ react-basics/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Chai from "./chai";
+
+const App = () => {
+  return (
+    <>
+      {/* // <div>Chai aur react with vite | Kartikey Ji</div> */}
+      <Chai />
+    </>
+  );
+};
+
+export default App;
--- readme.md
+++ readme.md
@@ -1,1 +1,6 @@
 ## React with chai aur code
+
+
+link["https://github.com/hiteshchoudhary/chai-aur-react]
+
+link for repository of react.
