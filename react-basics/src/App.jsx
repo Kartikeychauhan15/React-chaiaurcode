778cb3a901954ebe995afdc207ee7f8173db8658
Kartikey chauhan <itskartikeyy15@gmail.com>
2025-08-10 13:27:23 +0530

    create own react library and jsx

--- react-basics/src/App.jsx
+++ react-basics/src/App.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import Chai from "./chai";
 
 const App = () => {
+  const username = "chai aur code"
   return (
     <>
-      {/* // <div>Chai aur react with vite | Kartikey Ji</div> */}
+     <h1>Chhai aur react {username}</h1>
+       <div>Chai aur react with vite | Kartikey Ji</div>
       <Chai />
     </>
   );
--- readme.md
+++ readme.md
@@ -1,6 +1,11 @@
 ## React with chai aur code
 
 
-link["https://github.com/hiteshchoudhary/chai-aur-react]
+link["https://github.com/hiteshchoudhary/chai-aur-react"]
 
-link for repository of react.
+link for repository of react./
+
+
+Also see the opensource github link of facebook/react
+
+to Know more about react
