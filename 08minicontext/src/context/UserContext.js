abcad401cb3e9f5277bba7acbe80a00c3bfe4b20
Kartikey chauhan <itskartikeyy15@gmail.com>
2025-08-11 23:45:44 +0530

    context api discussion/project

---
+++ 08minicontext/src/context/UserContext.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const UserContext = React.createContext()
+
+export default UserContext;
+
+
+// like this we create usercontext 
+{/* <UserContext>
+<Login/>
+<Card/>
+<Header/>
+<UserContext/> */}
