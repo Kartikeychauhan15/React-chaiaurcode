c67c5e753e316137df330f7778ced2a9e1ee955b
Kartikey chauhan <itskartikeyy15@gmail.com>
2025-08-13 18:34:58 +0530

    mega blog project progress

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,12 +1,42 @@
-import React from 'react'
+import React, { useState , useEffect} from 'react'
+import {useDispatch} from "react-redux";
+import {login, logout} from "./store/authSlice"
+import authService from "./appwrite/auth"
+import { Footer, Header } from './components';
 
 const App = () => {
-  // console.log(process.env.REACT_APP_APPWRITE_URL); // yeh nahi chalega
-  console.log(import.meta.env.VITE_APPWRITE_URL)
+   const [loading, setLoading] = useState(true);
+   const dispatch = useDispatch();
 
-  return (
-    <div>Blog with appwrite</div>
-  )
+   useEffect(()=>{
+    authService.getCurrentUser()
+      .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}));
+      }else{
+        dispatch(logout());
+      }
+    })
+    .catch((error)=>{
+      console.error("Error fetching user data:", error);
+    })
+    .finally(()=> setLoading(false))
+   },[])
+
+
+
+  // console.log(process.env.REACT_APP_APPWRITE_URL); // yeh nahi chalega
+  // console.log(import.meta.env.VITE_APPWRITE_URL)
+
+  return !loading ? <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+    <div className="w-full block">
+      <Header />
+      <main>
+         TODO: {/* <Outlet/> */}
+      </main>
+      <Footer/>
+    </div>
+  </div> : (null)
 }
 
 export default App
--- 12MegaBlog/src/appwrite/auth.js
+++ 12MegaBlog/src/appwrite/auth.js
@@ -62,5 +62,5 @@ export class AuthService {
 
 const authService = new AuthService();
 
-export default AuthService;
+export default authService;
 
